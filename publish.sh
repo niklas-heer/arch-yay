16c35a6f15bdc5f28887d27393097667220c477e
Niklas Heer <niklas.heer@gmail.com>
2018-03-03 23:56:00 +0100

    Add publish script to push it to the docker hub

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Description: This script publishes a new version to the Docker Hub.
+# Author: Niklas Heer (niklas.heer@gmail.com)
+# Version: 1.0.0 (2018-03-03)
+
+main() {
+    HUB_BASE="niklasheer"
+    LOCAL_NAME="$1"
+    HUB_NAME="$1"
+    VERSION="$2"
+
+    docker login
+    docker tag "$LOCAL_NAME" "$HUB_BASE/$HUB_NAME:$VERSION"
+    docker push "$HUB_BASE/$HUB_NAME:$VERSION"
+}
+
+main "$@"
